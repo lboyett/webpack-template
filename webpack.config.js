388350c393d2869ca54e9f1847d8368544f7a217
Luke Boyett <lboyett@gmail.com>
2022-12-10 10:08:31 +0100

    initialze webpack template up to npm run watch

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
+      },
+      devtool: 'inline-source-map',
+      plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+        }),
+      ],
+    output: {
+        filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+        ],
+      },
+};
+
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// module.exports = {
+//     mode: 'development',
+//     entry: {
+//         index: './src/index.js',
+//         print: './src/print.js',
+//       },
+//       devtool: 'inline-source-map',
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//      title: 'Development',
+//     }),
+//   ],
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/i,
+//         use: ['style-loader', 'css-loader'],
+//       },
+//     ],
+//     output: {
+//         filename: '[name].bundle.js',
+//         path: path.resolve(__dirname, 'dist'),
+//         clean: true,
+//       },
+//   },
+// };
